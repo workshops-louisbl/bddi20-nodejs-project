e8a32a5a3397f4b0ba2c8ffa14cec0ddc448389f
louisbl <louis@beltramo.me>
2021-01-20 14:20:24 +0100

    remove old setSearchRules

--- twitter.js
+++ twitter.js
@@ -2,9 +2,7 @@ const http = require("https")
 const { Writable, Readable, pipeline, Transform } = require("stream")
 
 const TWT_API_HOST = "api.twitter.com"
-const TWT_API_SAMPLE_PATH = "/2/tweets/sample/stream?tweet.fields=attachments,author_id,geo&expansions=author_id,attachments.media_keys&media.fields=url"
 const TWT_API_SEARCH_PATH = "/2/tweets/search/stream?tweet.fields=attachments&expansions=attachments.media_keys&media.fields=url"
-const TWT_API_RULES_PATH = "/2/tweets/search/stream/rules"
 const BEARER_TOKEN = process.env.TWT_BEARER_TOKEN
 
 const options = {
@@ -39,35 +37,7 @@ function connectToTwitter() {
   req.end()
 }
 
-function setSearchRules(rules) {
-  const opts = {
-    ...options,
-    path: TWT_API_RULES_PATH,
-    method: "POST",
-  }
-
-  const data = JSON.stringify({
-    add: rules
-  })
-
-  console.log("body data: ", data)
-
-  const req = http.request(opts, (res) => {
-    let data
-    res.on("data", (chunk) => {
-      data += chunk
-    })
-    res.on("end", () => {
-      console.log("twt api response: ", data)
-    })
-  })
-
-  req.write(data)
-  req.end()
-}
-
 module.exports = {
   tweetStream,
   connectToTwitter,
-  setSearchRules
 }
